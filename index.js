46e0cb7a6bafcaa80e1dff649c5a1d1878faaf63
SamsonKwizela <142597868+SamsonKwizela@users.noreply.github.com>
2024-09-26 16:12:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const elements = document.querySelectorAll('.main_content');
+
+    window.addEventListener('scroll', () => {
+        const windowHeight = window.innerHeight;
+
+        elements.forEach(element => {
+            const position = element.getBoundingClientRect().top;
+
+            if (position < windowHeight - 100) {
+                element.classList.add('fade-slide');
+            }
+        });
+    });
+});
 
